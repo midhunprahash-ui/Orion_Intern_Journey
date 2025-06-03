41bf2d7c309966ff4186209f07096116ef8c4c34
Midhun Prahash SR <midhun.prahash@excelschools.edu.in>
2025-06-03 14:11:22 +0530

    feat: refactor CSV to JSON conversion to use custom parsing instead of pandas

--- TASK 1/csv_to_json.py
+++ TASK 1/csv_to_json.py
@@ -1,6 +1,41 @@
-import pandas as pd
-# Read the CSV file
-df =pd.read_csv('employees.csv')
+def format_value(value):
 
-# Convert to JSON file
-df.to_json('employees.json', orient='records', lines=True, index=False)
+    if value.replace('.', '').isdigit():
+        return value  
+    return f'"{value}"' 
+
+def csv_to_json(csv_file, json_file):
+
+    json_data = '['
+    
+    with open(csv_file, 'r') as file:
+    
+        headers = file.readline().strip().split(',')
+        
+        
+        first_record = True
+        for line in file:
+            values = line.strip().split(',')
+            
+            
+            if not first_record:
+                json_data += ','
+            first_record = False
+            
+            
+            json_data += '\n  {'
+            for i, value in enumerate(values):
+                if i > 0:  
+                    json_data += ','
+                json_data += f'\n    "{headers[i]}": {format_value(value)}'
+            json_data += '\n  }'
+    
+
+    json_data += '\n]'
+    
+    
+    with open(json_file, 'w') as file:
+        file.write(json_data)
+
+
+csv_to_json('employees.csv', 'employees.json')
