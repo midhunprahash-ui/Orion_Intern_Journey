b58939d3fbc47f9febbaefd7ccb3f2ea004fa1c9
Midhun Prahash SR <midhun.prahash@excelschools.edu.in>
2025-06-05 17:10:18 +0530

    fix: update model evaluation to include predictions and classification report

--- TASK_2/train_model.py
+++ TASK_2/train_model.py
@@ -4,6 +4,7 @@ from thefuzz import fuzz
 import jellyfish
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 import joblib
 
 # Connecting to the db
@@ -51,6 +52,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 rf = RandomForestClassifier(n_estimators=100, random_state=42)
 rf.fit(X_train, y_train)
 
-# Saving the model
+# Evaluating the model 
 
-joblib.dump(rf, 'name_matching_model.pkl')
+y_pred = rf.predict(X_test)
+print(classification_report(y_test, y_pred))
