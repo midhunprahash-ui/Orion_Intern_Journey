9fd6e812241e18ce5aea60a1e5afa653129cf499
Midhun Prahash SR <midhun.prahash@excelschools.edu.in>
2025-06-16 15:50:33 +0530

    feat: implement Google Drive API integration for file listing and credential management

---
+++ TASK_3/api_fetch.py
@@ -0,0 +1,67 @@
+import pickle
+import os
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from tabulate import tabulate
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
+def get_gdrive_service():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    # return Google Drive API service
+    return build('drive', 'v3', credentials=creds)
+
+def list_files(items):
+    """given items returned by Google Drive API, prints them in a tabular way"""
+    if not items:
+        # empty drive
+        print('No files found.')
+    else:
+        rows = []
+        for item in items:
+            # get the File ID
+            id = item["id"]
+            # get the name of file
+            name = item["name"]
+            try:
+                # parent directory ID
+                parents = item["parents"]
+            except:
+                # has no parrents
+                parents = "N/A"
+            try:
+                # get the size in nice bytes format (KB, MB, etc.)
+                size = get_size_format(int(item["size"]))
+            except:
+                # not a file, may be a folder
+                size = "N/A"
+            # get the Google Drive type of file
+            mime_type = item["mimeType"]
+            # get last modified date time
+            modified_time = item["modifiedTime"]
+            # append everything to the list
+            rows.append((id, name, parents, size, mime_type, modified_time))
+        print("Files:")
+        # convert to a human readable table
+        table = tabulate(rows, headers=["ID", "Name", "Parents", "Size", "Type", "Modified Time"])
+        # print the table
+        print(table)
