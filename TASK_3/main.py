6fe94b666f90f55b96a355bc2248a678d6349fe5
Midhun Prahash SR <midhun.prahash@excelschools.edu.in>
2025-06-16 13:08:58 +0530

    feat: initialize main.py with data processing for execution status from multiple Excel files

---
+++ TASK_3/main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Create Dataframes for all the sheets
+
+df1 = pd.read_excel('/Users/midhun/Developer/Git/Orion_Intern_Journey/TASK_3/Files/10062025.xlsx')
+df2 = pd.read_excel('/Users/midhun/Developer/Git/Orion_Intern_Journey/TASK_3/Files/11062025.xlsx')
+df3 = pd.read_excel('/Users/midhun/Developer/Git/Orion_Intern_Journey/TASK_3/Files/12062025.xlsx')
+df4 = pd.read_excel('/Users/midhun/Developer/Git/Orion_Intern_Journey/TASK_3/Files/13062025.xlsx')
+df5 = pd.read_excel('/Users/midhun/Developer/Git/Orion_Intern_Journey/TASK_3/Files/14062025.xlsx')
+df6 = pd.read_excel('/Users/midhun/Developer/Git/Orion_Intern_Journey/TASK_3/Files/15062025.xlsx')
+
+#
+
+c1=df1['Execution Status']
+c2=df2['Execution Status']
+c3=df3['Execution Status']
+c4=df4['Execution Status']
+c5=df5['Execution Status']
+c6=df6['Execution Status']
+
+
+
+count1 = df1['Execution Status'].value_counts()
+count2 = df2['Execution Status'].value_counts()
+count3 = df3['Execution Status'].value_counts()
+count4 = df4['Execution Status'].value_counts()
+count5 = df5['Execution Status'].value_counts()
+count6 = df6['Execution Status'].value_counts()
+
+
